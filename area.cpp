874e4c7159d2b2cf868910181fe220697cb29f3b
WAJIDRAVIAN <wajidh1997@gmail.com>
2017-03-27 15:16:22 +0500

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float r,pi,area;
+	pi=3.14;
+	cin>>r;
+	area=pi*r*r;
+	cout<<"\n area="<<area<<"\n \n hint: [using formula  (pi*r*r) where pi=3.14]";
+	return 0;
+}
